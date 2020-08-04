2348a9fd086082fed92f2638e58aae669b36ce47
Azat <yaakovazat@gmail.com>
2020-08-04 11:14:10 +0600

    unattached zsh for first run

--- zsh.sh
+++ zsh.sh
@@ -2,8 +2,9 @@ sudo apt install && sudo apt upgrade -y
 sudo apt install git -y
 sudo apt install zsh -y 
 sudo apt install curl -y 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 echo "plugins=(git zsh-completions zsh-autosuggestions zsh-syntax-highlighting)"
+chsh -s $(which zsh)
